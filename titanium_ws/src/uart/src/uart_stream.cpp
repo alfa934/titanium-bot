54d4f7a4a383739640d148b25fa31b8a5e497473
alfa934 <alfarras934@gmail.com>
2025-07-17 21:39:10 +0700

    Update titanium_ws

---
+++ titanium_ws/src/uart/src/uart_stream.cpp
@@ -0,0 +1,189 @@
+#include "ros/ros.h"
+#include "robot_msgs/encoder.h"
+#include "robot_msgs/ultrasonic.h"
+#include "robot_msgs/motor.h"
+
+#include <iostream>
+#include <cstring>
+#include "uart/serialib.h"
+
+serialib serial;
+char tx_buffer[65]= "ABC";
+char rx_buffer[62];
+
+ros::Publisher pub_encoder, pub_ultrasonic;
+ros::Subscriber sub_motor;
+ros::Timer timer_serialWrite, timer_serialRead;
+
+robot_msgs::encoder encoder;
+robot_msgs::ultrasonic ultrasonic;
+
+
+void publishData()
+{
+    memcpy(&ultrasonic.ultrasonic_a, rx_buffer, 2);
+    memcpy(&ultrasonic.ultrasonic_b, rx_buffer + 2, 2);
+    memcpy(&ultrasonic.ultrasonic_c, rx_buffer + 4, 2);
+    memcpy(&ultrasonic.ultrasonic_d, rx_buffer + 6, 2);
+
+    memcpy(&encoder.enc_a, rx_buffer + 8, 2);
+    memcpy(&encoder.enc_b, rx_buffer + 10, 2);
+    memcpy(&encoder.enc_c, rx_buffer + 12, 2);
+    memcpy(&encoder.enc_x, rx_buffer + 14, 2);
+    memcpy(&encoder.enc_y, rx_buffer + 16, 2);
+
+    pub_ultrasonic.publish(encoder);
+    pub_encoder.publish(encoder);
+}
+
+
+void serialReadCallback(const ros::TimerEvent& event)
+{
+    static double last_serial_time = ros::Time::now().toSec();
+
+    try
+    {
+        if(serial.available())
+        {
+            // ROS_INFO_STREAM("buffer: " << serial.available());
+            static int receive_status;
+            static int header_status;
+            char data, data_head[1];
+
+            if(header_status == 0)
+            { 
+                if(serial.available() >= 3 + sizeof(rx_buffer)) //// 3 untuk header 't', 'e', 'l'
+                { 
+                    for(int i=0; i<3+sizeof(rx_buffer); i++)
+                    { 
+                        if(serial.readBytes(data_head, 1, 1, 1) >= 1)
+                        {
+                            data = data_head[0];
+                        }
+
+                        if(receive_status == 0 && data == 'A')
+                        {
+                            receive_status++;
+                        }
+                        else if(receive_status == 1 && data == 'B')
+                        {
+                            receive_status++;
+                        }
+                        else if(receive_status == 2 && data == 'C')
+                        {
+                            receive_status = 0;
+                            header_status = 1;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if(header_status == 1)
+            { 
+                char data_real[sizeof(rx_buffer)];
+
+                if(serial.available() >= sizeof(rx_buffer))
+                {
+                    if(serial.readBytes(data_real, sizeof(rx_buffer), 1, 1) >= 1)
+                    {
+                        memcpy(&rx_buffer, data_real, sizeof(rx_buffer));
+                    }
+
+                    publishData();
+
+                    header_status = 0;
+                }
+            }
+
+            last_serial_time = ros::Time::now().toSec();
+        }
+    }
+    catch (...)
+    {
+        encoder = robot_msgs::encoder();
+        ultrasonic = robot_msgs::ultrasonic();
+    }
+
+    //--- if serial disconnects
+    if(ros::Time::now().toSec() - last_serial_time > 0.5)
+    {
+        try
+        {
+            new (&serial) serialib;
+            serial.openDevice("/dev/ttyUSB-SERIAL", 115200);
+
+            if(serial.isDeviceOpen())
+            {
+                ROS_INFO_STREAM("serial communication restarted");
+            }
+        }
+        catch(...)
+        {
+            ROS_WARN_STREAM("serial communication failed. Try reconnect in 2 seconds");
+        }
+
+        encoder = robot_msgs::encoder();
+        ultrasonic = robot_msgs::ultrasonic();
+        
+        last_serial_time = ros::Time::now().toSec();
+    }
+    }
+
+
+void serialWriteCallback(const ros::TimerEvent &event)
+{
+    for(int i = 0; i < sizeof(tx_buffer); i++)
+    {
+        if(serial.isDeviceOpen())
+        {
+            try
+            {
+                serial.writeChar(tx_buffer[i]);
+            }
+            catch(...)
+            {
+                ROS_WARN_STREAM("Unable to send serial!");
+            }
+        }
+    }
+}
+
+void subMotorCallback(const robot_msgs::motorConstPtr &msg)
+{
+    memcpy(tx_buffer + 3, &msg->motor_a, 2);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "serialbot");
+    ros::NodeHandle nh;
+
+    sub_motor = nh.subscribe("/actuator/motor", 10, subMotorCallback);
+
+    pub_encoder = nh.advertise<robot_msgs::encoder>("/sensor/encoder", 10);
+    pub_ultrasonic = nh.advertise<robot_msgs::ultrasonic>("/sensor/ultrasonic", 10);
+
+    timer_serialWrite = nh.createTimer(ros::Duration(0.001), serialWriteCallback);
+    timer_serialRead = nh.createTimer(ros::Duration(0.001), serialReadCallback);
+
+    ///inisialisasi serial dengan mikrokontroller
+    try
+    {
+        serial.openDevice("/dev/ttyUSB-SERIAL", 115200);
+
+        if(serial.isDeviceOpen())
+        {
+            ROS_INFO_STREAM("Serial Port initialized");
+        }
+    }
+    catch (...)
+    {
+        ROS_ERROR_STREAM("Unable to open port, try to recconect");
+    }
+    /////end of inisialisasi////////
+
+    ros::spin();
+
+    return 0;
+}
---
+++ titanium_ws/src/udp/src/udp_stream.cpp
@@ -0,0 +1,195 @@
+#include "ros/ros.h"
+#include "robot_msgs/encoder.h"
+#include "robot_msgs/ultrasonic.h"
+
+ros::Publisher pub_encoder, pub_ultrasonic;
+ros::Timer timer_1ms;
+robot_msgs::encoder encoder;
+robot_msgs::ultrasonic ultrasonic;
+
+void timer1msCallback(const ros::TimerEvent &event)
+{
+    encoder.enc_a += 1;
+    encoder.enc_b += 2;
+    encoder.enc_c += 3;
+    encoder.enc_x += 4;
+    encoder.enc_y += 5;
+
+    pub_encoder.publish(encoder);
+
+    ultrasonic.ultrasonic_a += 10;
+    ultrasonic.ultrasonic_b += 20;
+    ultrasonic.ultrasonic_c += 30;
+    ultrasonic.ultrasonic_d += 40;
+    
+    pub_ultrasonic.publish(ultrasonic);
+}
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "pub_udp_node");
+    
+    ros::NodeHandle nh;
+
+    pub_encoder = nh.advertise<robot_msgs::encoder>("/sensor/encoder", 10);
+    pub_ultrasonic = nh.advertise<robot_msgs::ultrasonic>("/sensor/ultrasonic", 10);
+
+    timer_1ms = nh.createTimer(ros::Duration(0.001), timer1msCallback);
+
+    ROS_INFO("Publisher Node Started!");
+
+    ros::spin();
+    
+    return 0;
+}
+
+
+// #include "ros/ros.h"
+// #include "std_msgs/Float32MultiArray.h"
+
+// #include <cstring>
+// #include <unistd.h>
+// #include <sys/socket.h>
+// #include <arpa/inet.h>
+// #include <netinet/in.h>
+
+// #define STM32_ADDR 	"192.168.13.111"
+// #define STM32_PORT	1555
+// #define PC_ADDR		"192.168.13.100"
+// #define PC_PORT    	1556
+// #define BUFF_SIZE 	64
+
+// typedef struct
+// {
+// 	int16_t enc_a;
+// 	int16_t enc_b;
+// 	int16_t enc_c;
+
+// 	int16_t enc_x;
+// 	int16_t enc_y;
+
+// 	float 	yaw_degree;
+
+// 	uint16_t ultrasonic[4];
+
+// } udpRx_t ;
+
+// typedef struct
+// {
+// 	int16_t motor_a;
+// 	int16_t motor_b;
+// 	int16_t motor_c;
+// } udpTx_t ;
+
+// udpTx_t udp_tx;
+// udpRx_t udp_rx;
+
+// int sockfd;
+// char rx_buffer[BUFF_SIZE];
+// char tx_buffer[BUFF_SIZE] = "ABC";
+// struct sockaddr_in servaddr, cliaddr;
+
+// ros::Timer timer_1ms;
+// ros::Publisher pub;
+// ros::Subscriber sub;
+
+
+// void timerCallback1ms(const ros::TimerEvent &event)
+// {
+// 	socklen_t len = sizeof(cliaddr);
+// 	int bytes_captured = recvfrom(sockfd, rx_buffer, BUFF_SIZE, MSG_DONTWAIT, 
+// 						 (struct sockaddr *)&cliaddr, &len);
+
+// 	if (bytes_captured > 0)
+// 	{
+// 		memcpy(&udp_rx, rx_buffer +  3, sizeof(udpRx_t));
+
+// 		ROS_INFO("ENC A,B,C: %d; %d; %d", udp_rx.enc_a, udp_rx.enc_b, udp_rx.enc_c);
+// 		ROS_INFO("ENC X,Y: %d; %d", udp_rx.enc_x, udp_rx.enc_y);
+// 		ROS_INFO("YAW DEG: %.2f", udp_rx.yaw_degree);
+// 		ROS_INFO("ULTRA 0,1,2,3: %d; %d; %d; %d", udp_rx.ultrasonic[0], udp_rx.ultrasonic[1], udp_rx.ultrasonic[2], udp_rx.ultrasonic[3]);
+// 		ROS_INFO("==============================");
+
+// 		std_msgs::Float32MultiArray msg;
+
+// 		msg.data.clear();
+// 		msg.data.push_back((float)udp_rx.enc_a);
+// 		msg.data.push_back((float)udp_rx.enc_b);
+// 		msg.data.push_back((float)udp_rx.enc_c);
+// 		msg.data.push_back((float)udp_rx.enc_x);
+// 		msg.data.push_back((float)udp_rx.enc_y);
+// 		msg.data.push_back((float)udp_rx.yaw_degree);
+// 		msg.data.push_back((float)udp_rx.ultrasonic[0]);
+// 		msg.data.push_back((float)udp_rx.ultrasonic[1]);
+// 		msg.data.push_back((float)udp_rx.ultrasonic[2]);
+// 		msg.data.push_back((float)udp_rx.ultrasonic[3]);
+		
+// 		pub.publish(msg);
+// 	}
+
+// }
+
+// void dataSubCallback(const std_msgs::Float32MultiArray::ConstPtr &msg)
+// {
+// 	udp_tx.motor_a = (int16_t)msg->data[0];
+// 	udp_tx.motor_b = (int16_t)msg->data[1];
+// 	udp_tx.motor_c = (int16_t)msg->data[2];
+	
+// 	memcpy(tx_buffer +  3, &udp_tx, sizeof(udpTx_t));
+	
+//     socklen_t len = sizeof(cliaddr);
+// 	sendto(sockfd, tx_buffer, sizeof(tx_buffer), MSG_CONFIRM,
+// 			(const struct sockaddr *) &cliaddr, len); 
+// }
+
+// void initSocket()
+// {
+//     if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+//     {
+//         ROS_ERROR("SOCKET CREATION FAILED!!!");
+//         exit(EXIT_FAILURE);
+//     }
+    
+//     memset(&servaddr, 0, sizeof(servaddr));
+//     servaddr.sin_family = AF_INET; 
+//     servaddr.sin_addr.s_addr = inet_addr(PC_ADDR);
+//     servaddr.sin_port = htons(PC_PORT);
+
+//     memset(&cliaddr, 0, sizeof(cliaddr));
+//     cliaddr.sin_family = AF_INET;
+//     cliaddr.sin_addr.s_addr = inet_addr(STM32_ADDR);
+//     cliaddr.sin_port = htons(STM32_PORT);
+    
+//     if(bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+//     {
+//         ROS_ERROR("BIND FAILED!!!");
+//         close(sockfd);
+//         exit(EXIT_FAILURE);
+//     }
+
+//     ROS_INFO("UDP Server running on %s:%d", inet_ntoa(servaddr.sin_addr), PC_PORT);
+// }
+
+// int main(int argc, char **argv) 
+// {
+//     ros::init(argc, argv, "udp_node");
+
+//     ros::NodeHandle nh;
+
+//     ros::AsyncSpinner spinner(0);
+
+//     initSocket();
+
+//     pub = nh.advertise<std_msgs::Float32MultiArray>("/stm32_to_ros", 100);
+//     sub = nh.subscribe("/ros_to_stm32", 100, dataSubCallback);
+//     timer_1ms = nh.createTimer(ros::Duration(0.001), timerCallback1ms);
+
+//     spinner.start();
+
+//     ros::waitForShutdown();
+
+//     close(sockfd);
+
+//     return 0;
+// }
