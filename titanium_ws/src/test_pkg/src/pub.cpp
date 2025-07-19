e829a88a42454c46646afaf5982a04a150fb3e24
alfa934 <alfarras934@gmail.com>
2025-07-19 12:45:04 +0700

    Update titanium_ws

---
+++ titanium_ws/src/test_pkg/src/pub.cpp
@@ -0,0 +1,45 @@
+#include "ros/ros.h"
+#include "robot_msgs/encoder.h"
+#include "robot_msgs/ultrasonic.h"
+
+ros::Publisher pub_encoder, pub_ultrasonic;
+ros::Timer timer_1ms;
+robot_msgs::encoder encoder;
+robot_msgs::ultrasonic ultrasonic;
+
+void timer1msCallback(const ros::TimerEvent &event)
+{
+    encoder.enc_a += 1;
+    encoder.enc_b += 2;
+    encoder.enc_c += 3;
+    encoder.enc_x += 4;
+    encoder.enc_y += 5;
+
+    pub_encoder.publish(encoder);
+
+    ultrasonic.ultrasonic_a += 10;
+    ultrasonic.ultrasonic_b += 20;
+    ultrasonic.ultrasonic_c += 30;
+    ultrasonic.ultrasonic_d += 40;
+    
+    pub_ultrasonic.publish(ultrasonic);
+}
+
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "test_pub_node");
+    
+    ros::NodeHandle nh;
+
+    pub_encoder = nh.advertise<robot_msgs::encoder>("/sensor/encoder", 10);
+    pub_ultrasonic = nh.advertise<robot_msgs::ultrasonic>("/sensor/ultrasonic", 10);
+
+    timer_1ms = nh.createTimer(ros::Duration(0.001), timer1msCallback);
+
+    ROS_INFO("Publisher Node Started!");
+
+    ros::spin();
+    
+    return 0;
+}
