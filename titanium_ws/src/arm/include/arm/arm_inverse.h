499ca24fcec3bdc507aa81f3bf18403d0fafe88e
algieekaputra <estgie16@gmail.com>
2025-07-20 20:13:37 +0700

    update titanium_ws

--- titanium_ws/src/arm/include/arm/arm_inverse.h
+++ titanium_ws/src/arm/include/arm/arm_inverse.h
@@ -13,12 +13,7 @@ typedef struct
 } inv_output_t;
 
 
-void get_pos_output(inv_output_t *pos_output, float x_input, float y_input)
-{
-    pos_output -> length_pos_px = sqrt((x_input * x_input) + (y_input * y_input));
-    pos_output -> theta_pos_deg = (atan(x_input / y_input)) * (180/M_PI); //tihs outputs radian, not degrees
-}
-
+void get_pos_output(inv_output_t *pos_output, float x_input, float y_input);
 
 
 #endif
--- titanium_ws/src/arm/src/arm_inverse.cpp
+++ titanium_ws/src/arm/src/arm_inverse.cpp
@@ -1,1 +1,7 @@
 #include "arm/arm_inverse.h"
+
+void get_pos_output(inv_output_t *pos_output, float x_input, float y_input)
+{
+    pos_output -> length_pos_px = sqrt((x_input * x_input) + (y_input * y_input));
+    pos_output -> theta_pos_deg = (atan(x_input / y_input)) * (180/M_PI); //tihs outputs radian, not degrees
+}
