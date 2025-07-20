07dd7cfff11abf8380e5d1dc7a17621dbd645d8b
algieekaputra <estgie16@gmail.com>
2025-07-20 20:44:05 +0700

    update arm inverse

--- titanium_ws/src/arm/src/arm_inverse.cpp
+++ titanium_ws/src/arm/src/arm_inverse.cpp
@@ -3,5 +3,14 @@
 void get_pos_output(inv_output_t *pos_output, float x_input, float y_input)
 {
     pos_output -> length_pos_px = sqrt((x_input * x_input) + (y_input * y_input));
-    pos_output -> theta_pos_deg = (atan(x_input / y_input)) * (180/M_PI); //tihs outputs radian, not degrees
+    pos_output -> theta_pos_deg = (atan(x_input / y_input)) * (180/M_PI);
+    
+    if(x_input == 0)
+    {
+        pos_output -> theta_pos_deg = 90;
+    }
+    else if(x_input < 0)
+    {
+        pos_output -> theta_pos_deg = 180 - (pos_output -> theta_pos_deg);
+    }
 }
