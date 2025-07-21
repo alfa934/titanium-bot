06db173a4fef75c378ade4a8ad273f100ee6133f
algieekaputra <142159608+algieekaputra@users.noreply.github.com>
2025-07-21 12:29:32 +0700

    Update arm_inverse.cpp

--- titanium_ws/src/arm/src/arm_inverse.cpp
+++ titanium_ws/src/arm/src/arm_inverse.cpp
@@ -3,7 +3,7 @@
 void get_pos_output(inv_output_t *pos_output, float x_input, float y_input)
 {
     pos_output -> length_pos_px = sqrt((x_input * x_input) + (y_input * y_input));
-    pos_output -> theta_pos_deg = (atan(x_input / y_input)) * (180/M_PI);
+    pos_output -> theta_pos_deg = (acos(x_input / pos_output -> length_pos_px)) * (180/M_PI);
     
     if(x_input == 0)
     {
