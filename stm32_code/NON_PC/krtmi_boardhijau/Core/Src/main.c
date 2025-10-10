092b92b092b5054040c7e8d51846a8e9945b134c
alfa934 <alfarras934@gmail.com>
2025-10-10 17:57:05 +0700

    Just FCKING PUSH

--- stm32_code/NON_PC/krtmi_boardhijau/Core/Src/main.c
+++ stm32_code/NON_PC/krtmi_boardhijau/Core/Src/main.c
@@ -356,7 +356,7 @@ int16_t Controller_Drift(int16_t value, int16_t max)
     }
 }
 
-int16_t map(int16_ x, int16_ in_min, int16_ in_max, int16_ out_min, int16_ out_max)
+int16_t map(int16_t x, int16_t in_min, int16_t in_max, int16_t out_min, int16_t out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
